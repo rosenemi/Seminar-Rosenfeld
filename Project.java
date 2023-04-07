8dc35929cf18c8132b624454b7455f06e051fd60
rosenemi <98037718+rosenemi@users.noreply.github.com>
2023-04-07 12:19:59 -0400

    Add files via upload
    Added Project and Project Data

---
+++ Project.java
@@ -0,0 +1,10 @@
+public class Project{
+  int interest;
+  String projectName;
+  int projectNum;
+  public Project(String p, int pN){
+    interest = 0;
+    projectName = p;
+    projectNum = pN;
+  }
+}
