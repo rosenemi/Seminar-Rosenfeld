a16a65e70ab7becc067b55dce2b15d8a42c76e7f
rosenemi <98037718+rosenemi@users.noreply.github.com>
2023-04-10 09:38:14 -0400

    Update Main.java
    added

--- Main.java
+++ Main.java
@@ -2,6 +2,8 @@ class Main {
   public static void main(String[] args) {
     Seminar hope = new Seminar();
     hope.Upload();
+    hope.UploadProjects();
     hope.CalcInterest();
+    hope.sortProjects();
   }
 }
