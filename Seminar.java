9ab6d04c1561886ee190ae2c23c0e5d4a52445fa
rosenemi <98037718+rosenemi@users.noreply.github.com>
2023-04-10 09:36:40 -0400

    Update Seminar.java
    added sort and turned projects into an array instead of an arraylist

--- Seminar.java
+++ Seminar.java
@@ -5,8 +5,11 @@ import java.util.*;
 public class Seminar{
   int max = 74;
   int numS = 0;
+  int numProjects = 18;
+  //int[] interest = new int[numProjects];
   Student[] stu = new Student[max]; //Array of all the people registred to come to the party
   int[][] schedule = new int[5][5];
+  ArrayList<Project>  Projects = new ArrayList<Project>();
   public void Upload(){
     try {
       File myObj = new File("SrSeminar_Data.csv");
@@ -22,6 +25,7 @@ public class Seminar{
         int uChoice5 = Integer.parseInt(compS[6]);
         Student x = new Student(uStudentID, uName, uChoice1, uChoice2, uChoice3, uChoice4, uChoice5); //creates a new object, called student, for each line in the data file
         stu[numS] = x; // adds that People object to the array; the -1 is for indexing purposes
+        System.out.print(stu[numS].studentID + " " + stu[numS].choice1 + " " + stu[numS].choice2 + " " + stu[numS].choice3 + " " + stu[numS].choice4 + " " + stu[numS].choice5 + " ");
         numS++;
       }
       myReader.close();
@@ -31,7 +35,7 @@ public class Seminar{
       e.printStackTrace();
     }
   }
-    public void UploadProjects(){
+  public void UploadProjects(){
     try {
       File myObj = new File("Project_Data.csv");
       Scanner myReader = new Scanner(myObj);
@@ -54,7 +58,8 @@ public class Seminar{
     while (i < numS){
       int add = stu[i].choice1 - 1;
       if (add > -1){
-        interest[add] = interest[add] + 1;
+        Projects.get(add).updateInterest();
+        //interest[add] = interest[add] + 1;
       }
       i++;
     }
@@ -62,7 +67,7 @@ public class Seminar{
     while (i < numS){
       int add = stu[i].choice2 - 1;
       if (add > -1){
-        interest[add] = interest[add] + 1;
+        Projects.get(add).updateInterest();
       }
       i++;
     }
@@ -70,7 +75,7 @@ public class Seminar{
     while (i < numS){
       int add = stu[i].choice3 - 1;
       if (add > -1){
-        interest[add] = interest[add] + 1;
+        Projects.get(add).updateInterest();
       }
       i++;
     }
@@ -78,7 +83,7 @@ public class Seminar{
     while (i < numS){
       int add = stu[i].choice4 - 1;
       if (add > -1){
-        interest[add] = interest[add] + 1;
+        Projects.get(add).updateInterest();
       }
       i++;
     }
@@ -86,15 +91,21 @@ public class Seminar{
     while (i < numS){
       int add = stu[i].choice5 - 1;
       if (add > -1){
-        interest[add] = interest[add] + 1;
+        Projects.get(add).updateInterest();
       }
       i++; 
     }
     for (int x = 0; x < 18; x++){
-      System.out.println(interest[x]);
+      System.out.println(Projects.get(x).printInterest());
     }
   }
-  public void sort(){
+  public void sortProjects(){
+    Collections.sort(Projects);
+    for (int x = 0; x < 18; x++){
+      System.out.println(Projects.get(x).printInterest());
+    }
+  }
+  public void generateSchedule(){
     
   }
 }
