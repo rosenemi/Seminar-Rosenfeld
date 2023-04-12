026b110a7da4722bee44b3449a8cb3a89c88b965
rosenemi <98037718+rosenemi@users.noreply.github.com>
2023-04-11 22:48:38 -0400

    Update Project.java
    Final Version

--- Project.java
+++ Project.java
@@ -1,9 +1,9 @@
-import java.util.*;
 
 public class Project implements Comparable<Project> {
   int interest;
   String projectName;
   int projectNum;
+  int numOfSession = 0;
   public Project(String p, int pN){
     interest = 0;
     projectName = p;
@@ -12,11 +12,24 @@ public class Project implements Comparable<Project> {
   public void updateInterest() {
     interest = interest + 1;
   }
+  public void updateInterestII(){
+    interest = interest - 16;
+  }
   public int printInterest(){
     return interest;
+  }
+  public void addSession(){
+    numOfSession++;
+  }
+  public int numSession(){
+    return numOfSession;
   }
 
   public int compareTo(Project p) {
     return ((Project)p).interest - this.interest;
+  }
+
+  public String toString() {
+    return "" + projectNum;
   }
 }
