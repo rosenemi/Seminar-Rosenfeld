1397b01becb11bd47842399343faedf26db98516
rosenemi <98037718+rosenemi@users.noreply.github.com>
2023-04-11 22:47:01 -0400

    Update Main.java
    Final Version

--- Main.java
+++ Main.java
@@ -5,5 +5,16 @@ class Main {
     hope.UploadProjects();
     hope.CalcInterest();
     hope.sortProjects();
+    hope.generateSchedule();
+    hope.sortStudents();
+    hope.fillIn(0);
+    hope.fillIn(1);
+    hope.fillIn(2);
+    hope.fillIn(3);
+    hope.fillIn(4);
+    hope.printStu();
+    int a = hope.getConflicts();
+    System.out.println("Total Conflicts: " + a);
+    hope.printSession();
   }
 }
